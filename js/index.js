4436c1e14e1545274de581c844a7d2f3a6d27148
Nano204 <fgomeztenorio@hotmail.com>
2022-10-09 11:08:48 -0500

    Fueature: Format AM/PM-24H toggle added Maintenance: Modularized functions for cleaner code.

--- js/index.js
+++ js/index.js
@@ -1,36 +1,63 @@
-const onLoading =() => {   
-     setInterval(()=>{
-        let d = new Date();
-        let hours = d.getHours(); 
-        let minutes = d.getMinutes();
-        let seconds = d.getSeconds();
-       
-        let time = document.getElementById("time");
-        let second = document.getElementById("second");
-        let minute = document.getElementById("minute");
-        let hour = document.getElementById("hour");
-        let secondangel = seconds * 6;
-        let minute_angel = minutes * 6 ;
-        let hour_angel = hours * 6;
-        if (hours > 12 ){
-            hour_angel = (hours - 12)*30 + (minutes * 0.5);
-        }else{
-            hour_angel = (hours)*30;
-        }            
-        if (seconds < 10){
-            seconds = `0${seconds}`
-        }
-        if (minutes < 10){
-            minutes = `0${minutes}`
-        }
-        if (hours < 10){
-            hours = `0${hours}`
-        }
-        time.innerHTML=`${hours}:${minutes}:${seconds}`
-        second.style.transform=`rotate(${secondangel}deg)`
-        minute.style.transform=`rotate(${minute_angel}deg)`
-        hour.style.transform=`rotate(${hour_angel}deg)`       
+//Main functions directly called by HTML index
+const onLoading = () => {
+  setInterval(() => {
+    const time = getTime();
+    const { timeHTML, hoursHTML, minutesHTML, secondsHTML } = getVisualElements();
+    updateAnalogTime(time, { hoursHTML, minutesHTML, secondsHTML });
+    updateDigitalTime(time, timeHTML, format);
+  }, 1000);
+};
 
-     }, 1000); 
-    
+let format;
+const toogle = () => {
+  const h24Button = document.getElementById("h24Button");
+  const ampmButton = document.getElementById("ampmButton");
+  ampmButton.classList.contains("activeButton")
+    ? (ampmButton.classList.remove("activeButton"),
+      h24Button.classList.add("activeButton"),
+      (format = "24H"))
+    : (ampmButton.classList.add("activeButton"),
+      h24Button.classList.remove("activeButton"),
+      (format = "AM/PM"));
+};
+
+//Support functions used by mains
+const getTime = () => {
+  const d = new Date();
+  const hours = d.getHours();
+  const minutes = d.getMinutes();
+  const seconds = d.getSeconds();
+  return { hours, minutes, seconds };
+};
+
+const getVisualElements = () => {
+  const timeHTML = document.getElementById("time");
+  const hoursHTML = document.getElementById("hour");
+  const minutesHTML = document.getElementById("minute");
+  const secondsHTML = document.getElementById("second");
+  return { timeHTML, hoursHTML, minutesHTML, secondsHTML };
+};
+
+const updateAnalogTime = (
+  { hours, minutes, seconds },
+  { hoursHTML, minutesHTML, secondsHTML }
+) => {
+  const secondsAngle = seconds * 6;
+  const minutesAngle = minutes * 6;
+  const hoursAngle = hours * 30 + minutes * 0.5;
+
+  secondsHTML.style.transform = `rotate(${secondsAngle}deg)`;
+  minutesHTML.style.transform = `rotate(${minutesAngle}deg)`;
+  hoursHTML.style.transform = `rotate(${hoursAngle}deg)`;
+};
+
+const updateDigitalTime = ({ hours, minutes, seconds }, timeHTML, format) => {
+  let ampm;
+  if (format == "AM/PM") {
+    hours > 12 ? ((hours = hours - 12), (ampm = "PM")) : (ampm = "AM");
   }
+  if (hours < 10) hours = "0" + hours;
+  if (minutes < 10) minutes = "0" + minutes;
+  if (seconds < 10) seconds = "0" + seconds;
+  timeHTML.innerHTML = `${hours}:${minutes}:${seconds} ${format == "AM/PM" ? ampm : ""}`;
+};
